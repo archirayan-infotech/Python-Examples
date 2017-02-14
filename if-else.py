bf680fc6bfcd2fa6a3f1cbcdab78c5ce00fd8956
Archirayan Infotech <info@archirayan.com>
2017-02-14 15:45:54 +0530

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,7 @@
+name ="lucy"
+if name is "luchy":
+    print("i am lucy")
+elif name is "daku":
+    print("i am not lucy but i am daku")
+else:
+    print("Hey you are not lucy , lol you fool what are you doing here ?")
